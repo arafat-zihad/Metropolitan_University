686ece74b5692d1df68b725fd87890b2b5004661
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-23 21:53:59 +0600

    800 Cf rating p 1 solved

---
+++ Competitive_Programming/800_CF_Rating/P_01.cpp
@@ -0,0 +1,16 @@
+//https://codeforces.com/contest/1669/problem/A
+ 
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin >> t;
+    int a;
+    while(t--){
+        cin >> a; 
+        if(a >= 1900) cout << "Division 1" << endl;
+        else if(a >= 1600 && a <= 1899) cout << "Division 2" << endl;
+        else if(a >= 1400 && a <= 1599) cout << "Division 3" << endl;
+        else if(a <= 1399) cout << "Division 4" << endl;
+    }
+    return 0;
+}
