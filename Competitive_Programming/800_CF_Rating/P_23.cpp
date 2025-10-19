d072165c2f5f254ae92d3b272b95a7f52fa6d42e
arafat-zihad <arafat.zihad01@gmail.com>
2025-10-20 00:26:51 +0600

    800 solve 1

---
+++ Competitive_Programming/800_CF_Rating/P_23.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/problemset/problem/677/A
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, h; cin >> n >> h;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > h){
+            float o = (float)arr[i]/h;
+            cnt += ceil(o);
+            // cout << cnt << " ";
+        }
+        else {
+            cnt++; 
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+}
