0b007a421715f9ef7ef8e5eb8cda02bee6c5630c
arafat-zihad <arafat.zihad01@gmail.com>
2025-10-24 16:14:11 +0600

    900 3rd solved

--- Competitive_Programming/900_CF_Rating/P_03.cpp
+++ Competitive_Programming/900_CF_Rating/P_03.cpp
@@ -1,20 +1,13 @@
 //https://codeforces.com/problemset/problem/451/A
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n, m; cin >> n >> m;
 
-    int a = n + m;
-        int chk = 0; 
-
-    for(int i = 0; i < a; i++){
-        chk = a - 2;
-        if(chk < 2 && i % 2 != 0){
-            cout << "Akshat" << endl;
-        }
-        else if( chk < 2 && i % 2 == 0){
-            cout << "Malvika" << endl;
-        }
-    }
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if (min(n, m) % 2 == 1)
+        cout << "Akshat" << endl;
+    else
+        cout << "Malvika" << endl;
     return 0;
 }
