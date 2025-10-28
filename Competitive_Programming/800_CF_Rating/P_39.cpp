d8a3afa3b646070d847b2707b44e18fd9d4fbf82
arafat-zihad <arafat.zihad01@gmail.com>
2025-10-28 19:27:15 +0600

    800

---
+++ Competitive_Programming/800_CF_Rating/P_39.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/617/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    int ans = (n + 4) / 5;
+
+    cout << ans << endl;
+    return 0;
+}
