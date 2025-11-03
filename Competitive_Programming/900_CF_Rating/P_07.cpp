ca959db0f91d905aa9fdc6c892f0ac9f1fb3974e
arafat-zihad <arafat.zihad01@gmail.com>
2025-11-03 14:25:18 +0600

    900 7th solved

--- Competitive_Programming/900_CF_Rating/P_07.cpp
+++ Competitive_Programming/900_CF_Rating/P_07.cpp
@@ -1,15 +1,17 @@
-//https://codeforces.com/problemset/problem/208/A
+//https://codeforces.com/problemset/problem/405/A
 
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    string  s; cin >> s;
-    string w = "WUB";
-
-    for(int i = 0; i < s.size(); i++){
-        if()
+    int n; cin >> n; 
+    vector <int> v(n); 
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
     }
+    sort(v.begin(), v.end());
 
-    
+    for(int i = 0; i < n; i++){
+        cout << v[i] << endl;
+    }
     return 0;
 }
