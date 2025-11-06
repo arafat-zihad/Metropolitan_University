44d47bf3104c27c61dae034648310d8fabc27921
arafat-zihad <arafat.zihad01@gmail.com>
2025-11-06 10:01:38 +0600

    ps

--- Competitive_Programming/800_CF_Rating/P_47.cpp
+++ Competitive_Programming/800_CF_Rating/P_47.cpp
@@ -2,5 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int m, n; cin >> m >> n; cout << floor((m*n) / 2) << endl; return 0;
+    int m, n; cin >> m >> n; 
+    cout << floor((m*n) / 2) << endl;
+    return 0;
 }
