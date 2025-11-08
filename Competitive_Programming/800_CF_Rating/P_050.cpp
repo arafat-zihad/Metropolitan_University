3ca66c00598841118bd99344a0f9b03d0273f2f5
arafat-zihad <arafat.zihad01@gmail.com>
2025-11-08 21:51:19 +0600

    50th 800

---
+++ Competitive_Programming/800_CF_Rating/P_050.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/486/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n; cin >> n;
+    int sum = 0;
+    if(n % 2 == 0){
+        cout << n / 2 << endl;
+    } else {
+        cout << -(n+1)/2 << endl;
+    }
+    return 0;
+}
