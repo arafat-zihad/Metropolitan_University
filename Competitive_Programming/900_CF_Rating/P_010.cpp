36797fb2a5ea23cbdab5f017a6ebdf70f26905d3
arafat-zihad <arafat.zihad01@gmail.com>
2025-11-12 21:07:27 +0600

    900 th solved

---
+++ Competitive_Programming/900_CF_Rating/P_010.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/problemset/problem/313/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    int l = n % 10;
+    int ll =  (n / 10) % 10;
+    if(n >= 0){
+        cout << n << endl;
+    }else{
+        
+        if(ll != 0 && ll < l){
+            cout << (n / 100) * 10 + (n % 10);
+        } else{
+             cout << n / 10;
+        }
+    }
+    return 0;
+}
