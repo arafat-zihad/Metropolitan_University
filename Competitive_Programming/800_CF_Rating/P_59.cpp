db13b10b5b4dc780b75509cda871b6889c0e8e70
arafat-zihad <arafat.zihad01@gmail.com>
2025-11-17 23:56:21 +0600

    59th 800

---
+++ Competitive_Programming/800_CF_Rating/P_59.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/1772/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; 
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        cout << (s[0] - '0') + (s[2] - '0') << endl;
+    }
+    return 0;
+}
